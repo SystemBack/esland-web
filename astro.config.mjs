93c922afa90d05d3ce6f2c6cb0f0b1c2117dcf42
Eduardo <eduardo.cerv.jua@gmail.com>
2024-05-10 17:30:59 -0600

    Add footer, i18n and new pages for navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  redirects: {
+    '/es': '/'
+  },
   i18n: {
     defaultLocale: "es",
     locales: ["es", "ca", "en"],
